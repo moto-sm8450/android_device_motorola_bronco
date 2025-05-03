16a3888c06066c6685840cf7e4cfb7eaed94cc7e
Michael Bestas <mkbestas@lineageos.org>
2025-05-03 13:39:20 +0300

    bronco: Disable ultrawide camera in Aperture
    Pictures don't focus and video recording crashes
    
    Change-Id: Ib32e4c48d7992f24b7691258d26032b1fdeb82cd

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ $(call inherit-product, device/motorola/sm8475-common/sm8475.mk)
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
+    LineageApertureAppDevice \
     LineageSdkDevice \
     LineageSystemUIDevice \
     SystemUIResDevice
